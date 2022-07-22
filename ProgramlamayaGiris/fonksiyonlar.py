128098689611960d0c0ca9accf21d76e8a2b3a84
Nida Kus <85497008+nidakus@users.noreply.github.com>
2022-07-22 22:58:10 +0300

    Fonksiyonlar

---
+++ ProgramlamayaGiris/fonksiyonlar.py
@@ -0,0 +1,13 @@
+def kredileriListele():
+    krediler = [
+    "Hızlı Kredi",
+    "Maaşını Halkbank'tan alanlara özel",
+    "Mutlu emekli ihtiyaç kredisi"]
+
+    for kredi in krediler:
+        print("<option>" + kredi + "<option>")
+
+kredileriListele()
+kredileriListele()
+kredileriListele()
+kredileriListele()
