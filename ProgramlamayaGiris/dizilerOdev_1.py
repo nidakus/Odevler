764f870c7ee8145a3d5783f5ee0eb32301ea3bc7
Nida Kus <85497008+nidakus@users.noreply.github.com>
2022-07-22 22:15:31 +0300

    Ödev 1

---
+++ ProgramlamayaGiris/dizilerOdev_1.py
@@ -0,0 +1,10 @@
+# Öğrencilerini listelemek isteyen bir öğretmeni yapınız. (Öğrenci isimleri)
+
+ogrenciler = ["Ali","Mehmet","Ayşe","Fatma"]
+
+print(ogrenciler)
+
+print(ogrenciler[0])
+print(ogrenciler[1])
+print(ogrenciler[2])
+print(ogrenciler[3])
