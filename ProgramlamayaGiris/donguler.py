af060ffd5109f1fa65423694bf15c66221d4722d
Nida Kus <85497008+nidakus@users.noreply.github.com>
2022-07-22 22:49:37 +0300

    Döngüler

---
+++ ProgramlamayaGiris/donguler.py
@@ -0,0 +1,18 @@
+krediler = [
+    "Hızlı Kredi",
+    "Maaşını Halkbank'tan alanlara özel",
+    "Mutlu emekli ihtiyaç kredisi"]
+
+for kredi in krediler:
+    print(kredi)
+
+for i in range(len(krediler)):
+    print(krediler[i])
+
+# 3'ten 10'a kadar yaz
+for i in range(3,10):
+    print(i)
+
+# 0'dan 10'a kadar 2'şer arttırarak yaz
+for i in range(0,10,2):
+    print(i)
