b54e8668e2aa9918897617d780b2da42ca0cb186
Nida Kus <85497008+nidakus@users.noreply.github.com>
2022-07-22 21:16:05 +0300

    Ödev 1

---
+++ ProgramlamayaGiris/sartBloklariOdev_1.py
@@ -0,0 +1,12 @@
+# 2 adet sayısal değişken tanımlayınız.
+# Bu sayılara istediğiniz değeri atayınız.
+# Bu sayılardan hangisi büyükse, ekrana yazdırınız.
+# Sayıları eşit verdiğinizde ekrana bir şey yazmaması gerekiyor.
+
+sayi1 = 11
+sayi2 = 11
+
+if (sayi1 > sayi2):
+    print(sayi1)
+elif(sayi2 > sayi1):
+    print(sayi2)
